79a47ccb554437733c0461146987d8d7efa902bc
Daksh Singh <63799338+evenrise01@users.noreply.github.com>
2025-05-06 23:41:43 +0530

    Changes to about me page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,12 +3,18 @@ import { Card, Carousel } from "@/components/ui/apple-cards-carousel";
 import { GradientText } from "@/components/ui/gradient-text";
 import { LampDemo } from "@/components/ui/lamp";
 import { Timeline } from "@/components/ui/timeline";
+import { Github, Linkedin, Twitter } from "lucide-react";
 import React from "react";
 
 const AboutPage = () => {
     const data = [
         {
-          title: "2024",
+          jobTitle: "Frontend Engineer",
+          companyName: "Vercel",
+          companyLogo: "/vercel.svg",
+          companyLink: "vercel.com",
+          location: "London, UK - Remote",
+          time: "2024 - Present",
           content: (
             <div>
               <p className="mb-8 text-xs font-normal text-neutral-800 md:text-sm dark:text-neutral-200">
@@ -208,7 +214,7 @@ const AboutPage = () => {
     <main>
       {/* <LampDemo/> */}
       <div className="relative mx-auto max-w-6xl px-4 pt-28 pb-12 md:pt-36 md:pb-20">
-        <h2 className="relative z-2 mb-20 text-4xl font-medium tracking-tight sm:text-5xl md:mb-24 md:text-6xl text-center">
+        <h2 className="relative z-2 mb-10 text-4xl font-medium tracking-tight sm:text-5xl md:mb-12 md:text-6xl text-center">
           <p className="mb-3 text-xs font-normal tracking-widest text-white/70 uppercase md:text-sm">
             More About Me
           </p>
@@ -226,7 +232,7 @@ const AboutPage = () => {
         {/* Modified layout with carousel on the right */}
         <div className="flex flex-col items-center justify-between gap-8 lg:flex-row mb-12">
           {/* About content - left side */}
-          <div className="relative z-5 mx-auto flex max-w-xl flex-col gap-y-8 text-base font-light tracking-wider text-white/70 lg:mx-0 lg:max-w-[550px] lg:text-lg">
+          <div className="relative z-5 mx-auto flex max-w-xl flex-col gap-y-8 text-base font-light tracking-wider text-white/70 lg:mx-0 lg:max-w-[550px] lg:text-lg text-center md:text-start">
             <p>
               I'm Daksh, a proactive full-stack developer passionate about creating dynamic web experiences. 
               From frontend to backend, I thrive on solving complex problems with clean, efficient code. 
@@ -239,32 +245,23 @@ const AboutPage = () => {
             <p>
               I believe in waking up each day eager to make a difference!
             </p>
-            <div className="flex gap-3 w-fit">
+            <div className="flex gap-3 w-fit mx-auto -mt-4 lg:mx-0">
               <button data-state="closed" data-slot="tooltip-trigger">
                 <a href="https://linkedin.com/in/daksh" target="_blank" rel="noopener noreferrer" className="text-neutral-300 transition-colors hover:text-neutral-100">
                   <span className="sr-only">LinkedIn</span>
-                  <svg stroke="currentColor" fill="none" strokeWidth="1.5" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" height="20" width="20" xmlns="http://www.w3.org/2000/svg" className="stroke-1">
-                    <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
-                    <rect width="4" height="12" x="2" y="9"></rect>
-                    <circle cx="4" cy="4" r="2"></circle>
-                  </svg>
+                  <Linkedin/>
                 </a>
               </button>
               <button data-state="closed" data-slot="tooltip-trigger">
                 <a href="https://github.com/daksh" target="_blank" rel="noopener noreferrer" className="text-neutral-300 transition-colors hover:text-neutral-100">
                   <span className="sr-only">GitHub</span>
-                  <svg stroke="currentColor" fill="none" strokeWidth="1.5" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" height="20" width="20" xmlns="http://www.w3.org/2000/svg" className="stroke-1">
-                    <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path>
-                    <path d="M9 18c-4.51 2-5-2-7-2"></path>
-                  </svg>
+                <Github/>
                 </a>
               </button>
               <button data-state="closed" data-slot="tooltip-trigger">
                 <a href="https://x.com/daksh" target="_blank" rel="noopener noreferrer" className="text-neutral-300 transition-colors hover:text-neutral-100">
                   <span className="sr-only">Twitter</span>
-                  <svg stroke="currentColor" fill="none" strokeWidth="1.5" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" height="20" width="20" xmlns="http://www.w3.org/2000/svg" className="stroke-1">
-                    <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"></path>
-                  </svg>
+                  <Twitter/>
                 </a>
               </button>
             </div>
--- components/ui/timeline.tsx
+++ components/ui/timeline.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { MapPin } from "lucide-react";
 import {
   useMotionValueEvent,
   useScroll,
@@ -8,7 +9,12 @@ import {
 import React, { useEffect, useRef, useState } from "react";
 
 interface TimelineEntry {
-  title: string;
+  time: string;
+  companyName: string;
+  companyLink: string;
+  companyLogo: string;
+  location: string;
+  jobTitle: string;
   content: React.ReactNode;
 }
 
@@ -34,54 +40,96 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
 
   return (
     <div
-      className="w-full bg-white dark:bg-neutral-950 font-sans md:px-10"
+      className="w-full bg-white dark:bg-neutral-950 font-sans px-4 md:px-8"
       ref={containerRef}
     >
-      {/* <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
-        <h2 className="text-lg md:text-4xl mb-4 text-black dark:text-white max-w-4xl">
-          Changelog from my journey
-        </h2>
-        <p className="text-neutral-700 dark:text-neutral-300 text-sm md:text-base max-w-sm">
-          I&apos;ve been working on Aceternity for the past 2 years. Here&apos;s
-          a timeline of my journey.
-        </p>
-      </div> */}
-
       <div ref={ref} className="relative max-w-7xl mx-auto pb-20">
         {data.map((item, index) => (
           <div
             key={index}
-            className="flex justify-start pt-10 md:pt-40 md:gap-10"
+            className="flex justify-start pt-12 md:pt-20 md:gap-8"
           >
-            <div className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
-              <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-white dark:bg-black flex items-center justify-center">
-                <div className="h-4 w-4 rounded-full bg-neutral-200 dark:bg-neutral-800 border border-neutral-300 dark:border-neutral-700 p-2" />
+            {/* Left side - Company details (sticky on desktop) */}
+            <div className="sticky hidden md:flex flex-col z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
+              <div className="absolute left-3 flex h-10 w-10 items-center justify-center rounded-full bg-white dark:bg-black">
+                <div className="h-4 w-4 rounded-full border border-neutral-300 bg-neutral-200 p-2 dark:border-neutral-700 dark:bg-neutral-800" />
               </div>
-              <h3 className="hidden md:block text-xl md:pl-20 md:text-5xl font-bold text-neutral-500 dark:text-neutral-500 ">
-                {item.title}
-              </h3>
+              <div className="md:pl-20">
+                <div className="mt-2 flex flex-col items-start gap-y-2 text-sm">
+                  <span className="text-xs text-muted-foreground font-medium tracking-wide uppercase">
+                    {item.time}
+                  </span>
+                  <p className="text-lg font-semibold text-black dark:text-white/90">
+                    {item.jobTitle}
+                  </p>
+                  <a
+                    href={item.companyLink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center gap-2 font-medium text-neutral-700 dark:text-neutral-300 hover:text-neutral-900 dark:hover:text-neutral-100 transition-colors"
+                  >
+                    <img
+                      src={item.companyLogo}
+                      alt={`${item.companyName} Logo`}
+                      className="h-5 w-5 rounded-sm object-contain"
+                    />
+                    <span>{item.companyName}</span>
+                  </a>
+                  <div className="flex items-center gap-2 text-neutral-600 dark:text-neutral-400 text-sm">
+                    <MapPin size={16} />
+                    <span>{item.location}</span>
+                  </div>
+                </div>
+              </div>
             </div>
 
-            <div className="relative pl-20 pr-4 md:pl-4 w-full">
-              <h3 className="md:hidden block text-2xl mb-4 text-left font-bold text-neutral-500 dark:text-neutral-500">
-                {item.title}
-              </h3>
-              {item.content}{" "}
+            {/* Right side - Content (mobile shows company details here) */}
+            <div className="relative pl-12 md:pl-4 w-full">
+              {/* Mobile-only company details */}
+              <div className="md:hidden mb-6 flex flex-col gap-2">
+                <span className="text-xs text-muted-foreground font-medium tracking-wide uppercase">
+                  {item.time}
+                </span>
+                <p className="text-lg font-semibold text-black dark:text-white/90">
+                  {item.jobTitle}
+                </p>
+                <a
+                  href={item.companyLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="flex items-center gap-2 font-medium text-neutral-700 dark:text-neutral-300 hover:text-neutral-900 dark:hover:text-neutral-100 transition-colors"
+                >
+                  <img
+                    src={item.companyLogo}
+                    alt={`${item.companyName} Logo`}
+                    className="h-5 w-5 rounded-sm object-contain"
+                  />
+                  <span>{item.companyName}</span>
+                </a>
+                <div className="flex items-center gap-2 text-neutral-600 dark:text-neutral-400 text-sm">
+                  <MapPin size={16} />
+                  <span>{item.location}</span>
+                </div>
+              </div>
+              
+              <div className="prose prose-sm dark:prose-invert max-w-none">
+                {item.content}
+              </div>
             </div>
           </div>
         ))}
+        
+        {/* Timeline line */}
         <div
-          style={{
-            height: height + "px",
-          }}
-          className="absolute md:left-8 left-8 top-0 overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-neutral-200 dark:via-neutral-700 to-transparent to-[99%]  [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] "
+          style={{ height: height + "px" }}
+          className="absolute left-6 md:left-8 top-0 overflow-hidden w-[2px] bg-gradient-to-b from-transparent via-neutral-200 dark:via-neutral-700 to-transparent [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)]"
         >
           <motion.div
             style={{
               height: heightTransform,
               opacity: opacityTransform,
             }}
-            className="absolute inset-x-0 top-0  w-[2px] bg-gradient-to-t from-purple-500 via-blue-500 to-transparent from-[0%] via-[10%] rounded-full"
+            className="absolute inset-x-0 top-0 w-[2px] bg-gradient-to-t from-blue-500 via-emerald-500 to-transparent rounded-full"
           />
         </div>
       </div>
